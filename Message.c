589790009d0b87440cfd77887c7efe92a8a9345b
gao <gxh.vic@gmail.com>
2019-11-02 21:58:50 -0300

    created the stuct of the message. Created the message.h and message.c Still need a solution of how to create linked list for an array of struct.

---
+++ Message.c
@@ -0,0 +1,8 @@
+/*
+ * Message.c
+ *
+ *  Created on: 2019?11?2?
+ *      Author: Victor
+ */
+
+
---
+++ Message.h
@@ -0,0 +1,13 @@
+/*
+ * Message.h
+ *
+ *  Created on: 2019?11?2?
+ *      Author: Victor
+ */
+
+#ifndef MESSAGE_H_
+#define MESSAGE_H_
+
+
+
+#endif /* MESSAGE_H_ */
