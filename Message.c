16e6566cbee88102784763a4988f7772277a2deb
gao <gxh.vic@gmail.com>
2019-11-03 14:27:22 -0400

    message header and code now it include all the structure and mailbox initialize function

--- Message.c
+++ Message.c
@@ -3,6 +3,23 @@
  *
  *  Created on: 2019?11?2?
  *      Author: Victor
+ *Purpose: these functions used for support IPC
  */
-
-
+#include "Message.h"
+#include "Process.h"
+#define max_Mailbox 10
+struct mailbox_list mbx[max_Mailbox];
+struct __message*head=NULL;
+struct __message*current,*prev;
+void Initialize_Mailbox(void){
+    int i=0;
+    for(i=0;i<max_Mailbox;i++){
+        /*before mailbox belong to same process
+         * their next, prev and pcb_ptr are all
+         * set to null
+         */
+        mbx[i].Next=NULL;
+        mbx[i].Prev=NULL;
+        mbx[i].pcb_ptr=NULL;
+    }
+}
--- Message.h
+++ Message.h
@@ -7,7 +7,25 @@
 
 #ifndef MESSAGE_H_
 #define MESSAGE_H_
-
-
+typedef struct __message{//contain 4 elements
+    unsigned long message_addr;//??not sure about this
+    unsigned int size;
+    //pointer, point to mailbox which send from.
+    mbx* sender_mbx;
+    //pointer, point to next message
+    msg* next_msg;
+} msg;
+typedef struct mailbox_list{//contain 5 elements
+    //mailbox list for several mbx belong to same process
+    struct mailbox_list*Next;
+    struct mailbox_list*Prev;
+    //pointer, point to pcb
+    struct __PCB* pcb_ptr;
+    //pointer, point to the message
+    struct __message* msg_ptr;
+    //pointer, point to the last message in one of the mailbox
+    struct __message* tail_msg;
+} ;
+extern void Initialize_Mailbox(void);
 
 #endif /* MESSAGE_H_ */
