abc380a5a4ced67f02a9f7b553949d9b43376567
Ray <ruiyuansu.ray@gmail.com>
2019-11-21 00:37:15 -0400

    Updated comments in main

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
  *Course: Real time system
  *Professor: Larry Hughes
  *Purpose: This is main function of the code, it calls the functions in specific sequence.
+ *		   This assignment is to simulate a multi-porcess mechine that allow each process 
+ *		   to change their priority, to terminate, to send and receive messages.
+ *		   Running process can be switched in certain period (1/100 sec in this assignment)
+ *		   depend on their priority. Higher priority process will always block lower priority
+ *		   process, unless it is terminated or blocked.
  *Author:	Ray		Su	B00634512
 			Victor	Gao	B00677182
  *Last day modified: Oct.23
