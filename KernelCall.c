d02bd1d47eaeefe4a04561672bf55ffe59645c42
Ray <ruiyuansu.ray@gmail.com>
2019-12-05 15:13:34 -0400

    Corrected UART setting

--- KernelCall.c
+++ KernelCall.c
@@ -28,7 +28,7 @@ void KernelInitialization()
 	/* Initialize UART */
 	UART0_Init();           // Initialize UART0
 	InterruptEnable(INT_VEC_UART0);       // Enable UART0 interrupts
-	//UART0_IntEnable(UART_INT_RX | UART_INT_TX); // Enable Receive and Transmit interrupts
+	UART0_IntEnable(UART_INT_RX | UART_INT_TX); // Enable Receive and Transmit interrupts
 
 	PENDSV_ON = FALSE;
 
